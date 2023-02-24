066985497ca9398cc52cd5fb94b3430f628fcd54
Данил Царев <Drsif@yandex.ru>
2023-02-25 00:35:54 +0300

    Добавлены urls и view-функции

---
+++ yatube/posts/urls.py
@@ -0,0 +1,7 @@
+from django.urls import path
+from . import views
+
+urlpatterns = [
+    path('', views.index),
+    path('group/<slug:pk>/', views.group_posts),
+]
--- yatube/posts/views.py
+++ yatube/posts/views.py
@@ -1,3 +1,11 @@
-from django.shortcuts import render
+from django.shortcuts import HttpResponse
 
 # Create your views here.
+
+
+def index(request):
+    return HttpResponse('Главная страница')
+
+
+def group_posts(request, pk):
+    return HttpResponse(f'Пост о судбьбе {pk}')
